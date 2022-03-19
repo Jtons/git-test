589c42ab2bcf4e277002d6be64218b21d486f5c3
Jtons <Jtons@qq.com>
2022-03-19 20:18:45 +0800

    second-commit

--- src/main/java/cn.edu.wxu/Git_test.java
+++ src/main/java/cn.edu.wxu/Git_test.java
@@ -3,5 +3,6 @@ package cn.edu.wxu;
 public class Git_test {
     public static void main (String[] args){
         System.out.println("hello git");
+        System.out.println("hello Jtons");
     }
 }
